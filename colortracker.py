c8c05dc0d022bacd0d61862da7cd16ca729ce4b9
johst529 <johst529@student.liu.se>
2017-10-21 00:36:59 +0200

    La till en enkel keybuffer analys

--- colortracker.py
+++ colortracker.py
@@ -1,7 +1,15 @@
 import numpy as np
 import cv2
-
 # import PyOpenPose
+
+def printKeyPointPositions(keypointList):
+    i = 1
+    for keyPoint in keypointList:
+        x = keyPoint.pt[0]
+        y = keyPoint.pt[1]
+        #print("KeyPoint nr:" + str(i) + "x:" + str(x) + "y:" + str(y) )
+        i = i + 1
+    print("Number of blobs =" + str(i))
 
 cap = cv2.VideoCapture('4farger.mp4')
 width = int(cap.get(3))
@@ -21,13 +29,19 @@ blob_params.filterByCircularity = False
 blob_params.minCircularity = 0.5
 blob_params.filterByInertia = True
 blob_params.minInertiaRatio = 0.5
-# blob_params.minDistBetweenBlobs = 300
+blob_params.minDistBetweenBlobs = 500
 blob_params.filterByArea = True
 blob_params.minArea = 60
+blob_params.maxArea = 200
 blob_params.filterByConvexity = True
 blob_params.minConvexity = 0.8
 blob_params.filterByColor = 0
 blob_params.blobColor = 100
+
+detector = cv2.SimpleBlobDetector_create(blob_params)
+
+cv2.namedWindow('Keypoints',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Keypoints', 800,600)
 
 while (cap.isOpened()):
 
@@ -45,15 +59,15 @@ while (cap.isOpened()):
         # h = np.multiply(h, (th2 / 255).astype(np.uint8), dtype='uint8')
 
         # define range of blue color in HSV
-        lower_pink = np.array([0, 50, 50])
+        lower_pink = np.array([150, 50, 50])
         upper_pink = np.array([180, 255, 255])
         # Threshold the HSV image to get only blue colors
         mask = cv2.inRange(hsv, lower_pink, upper_pink)
         # Bitwise-AND mask and original image
         res = cv2.bitwise_and(img, img, mask=mask)
 
-        detector = cv2.SimpleBlobDetector_create(blob_params)
-        filtered = cv2.GaussianBlur(res, (7, 7), 1)
+ 
+        filtered = cv2.GaussianBlur(res, (15, 15), 1)
         keypoints = detector.detect(filtered)
         # Draw detected blobs as red circles.
         # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
@@ -61,7 +75,7 @@ while (cap.isOpened()):
                                               cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         keybuffer = np.append(keybuffer, np.array(keypoints))
         # print(type(keybuffer[0][0]))
-
+        printKeyPointPositions(keybuffer)
         # Show keypoints
         cv2.imshow("Keypoints", im_with_keypoints)
         out.write(im_with_keypoints)
