dd1fb56ee59fcb6de3ab6477b61862a8b3eb706f
Rolf Lifvergren <rolli107@student.liu.se>
2017-10-20 15:51:39 +0200

    Lite kod och lite video

---
+++ colortracker.py
@@ -0,0 +1,92 @@
+import numpy as np
+import cv2
+
+# import PyOpenPose
+
+cap = cv2.VideoCapture('4farger.mp4')
+width = int(cap.get(3))
+height = int(cap.get(4))
+fps = cap.get(cv2.CAP_PROP_FPS)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('4farger.avi', fourcc, fps, (width, height))
+counter = 0
+keybuffer = np.array([])
+
+blob_params = cv2.SimpleBlobDetector_Params()
+blob_params.minThreshold = 10
+blob_params.maxThreshold = 180
+blob_params.thresholdStep = 30
+blob_params.minRepeatability = 1
+blob_params.filterByCircularity = False
+blob_params.minCircularity = 0.5
+blob_params.filterByInertia = True
+blob_params.minInertiaRatio = 0.5
+# blob_params.minDistBetweenBlobs = 300
+blob_params.filterByArea = True
+blob_params.minArea = 60
+blob_params.filterByConvexity = True
+blob_params.minConvexity = 0.8
+blob_params.filterByColor = 0
+blob_params.blobColor = 100
+
+while (cap.isOpened()):
+
+    ret, img = cap.read()
+    if ret == True:
+        #img = cv2.resize(img, None, fx=0.7, fy=0.7, interpolation=cv2.INTER_CUBIC)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # h, s, v = cv2.split(hsv)
+        # h,s gets lot of sample nose at low/high intensities
+        # vthresh = 40
+        # ret2, th2 = cv2.threshold(v, vthresh, 255, cv2.THRESH_BINARY)
+        # h = np.multiply(h, (th2 / 255).astype(np.uint8), dtype='uint8')
+        # ret2, th2 = cv2.threshold(v, 255 - vthresh, 255, cv2.THRESH_BINARY_INV)
+        # h = np.multiply(h, (th2 / 255).astype(np.uint8), dtype='uint8')
+
+        # define range of blue color in HSV
+        lower_pink = np.array([0, 50, 50])
+        upper_pink = np.array([180, 255, 255])
+        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(hsv, lower_pink, upper_pink)
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(img, img, mask=mask)
+
+        detector = cv2.SimpleBlobDetector_create(blob_params)
+        filtered = cv2.GaussianBlur(res, (7, 7), 1)
+        keypoints = detector.detect(filtered)
+        # Draw detected blobs as red circles.
+        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+        im_with_keypoints = cv2.drawKeypoints(filtered, keypoints, np.array([]), (0, 255, 0),
+                                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        keybuffer = np.append(keybuffer, np.array(keypoints))
+        # print(type(keybuffer[0][0]))
+
+        # Show keypoints
+        cv2.imshow("Keypoints", im_with_keypoints)
+        out.write(im_with_keypoints)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+    counter += 1
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+print('You did the thing :)')
+
+
+# Low scores if similar
+def simularityDistance(keypoint1, keypoint2, sizeweight):
+    x1, y1 = keypoint1.pt
+    x2, y2 = keypoint2.pt
+    d2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
+    s2 = (keypoint1.size - keypoint2.size) ** 2
+    return d2 + s2 * sizeweight
+
+
+def simularityMatrix(keypoints1, keypoints2):
+    keypoints1.repmat()
