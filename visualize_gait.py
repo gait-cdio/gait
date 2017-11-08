7457f0af37ad82249939908a7eeffdf704d64225
Magnus Selin <magse761@student.liu.se>
2017-11-08 11:50:40 +0100

    Add visualizations

---
+++ visualize_gait.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize_gait(gait, fig=None):
+    gait_diff = np.diff(gait)
+    
+    np.where()
+
+    if not fig:
+        fig = plt.figure()
+    
+
