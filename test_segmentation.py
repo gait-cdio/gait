0eaef8bfded187c6f83a38d66b5b247dbc017ff4
Gait CDIO <cleese@mendes>
2017-11-21 12:35:51 +0100

    Tuning of background modeling aswell as grabCut inital tests

--- test_segmentation.py
+++ test_segmentation.py
@@ -6,7 +6,7 @@ import sys
 
 from collections import namedtuple
 
-from colortracker import match_points, PointFeatures
+from colortracker import PointFeatures
 
 plt.ioff()
 # import PyOpenPose
@@ -19,7 +19,7 @@ tracker = namedtuple('tracker', ['tracker', 'lower_bound', 'upper_bound', 'name'
 if 'cached' in sys.argv and os.path.isfile(cache_filename):
     pointbuffer = np.load(cache_filename)
 else:
-    cap = cv2.VideoCapture(videoname + '.mp4')
+    cap = cv2.VideoCapture('input-videos/' + videoname + '.mp4')
 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -40,15 +40,28 @@ else:
     x_coords = []
     y_coords = []
 
+    initMask = np.zeros(img.shape[:2],np.uint8)
+    grabMask = np.zeros(img.shape[:2],np.uint8)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    kernel5 = np.ones((5, 5), np.uint8)
+    kernel3 = np.ones((3, 3), np.uint8)
+    kernel7 = np.ones((7, 7), np.uint8)
+
     backgroundModel = cv2.createBackgroundSubtractorMOG2()
 
     while (cap.isOpened()):
 
         ret, img = cap.read()
+        ret, img = cap.read()
+        ret, img = cap.read()
         if ret == True:
 
             mask = backgroundModel.apply(img)
-
+            #mask[np.where(mask == 127)] = 0
+            #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel3)
+            #mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel3)
+            #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel5)
             cv2.imshow('Keypoints',mask)
 
             if paused:
@@ -66,15 +79,18 @@ else:
         counter += 1
     counter = 0
     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    while (cap.isOpened()):
+    while (cap.isOpened()):#
 
         ret, img = cap.read()
         if ret == True:
 
             mask = backgroundModel.apply(img)
-
+            mask[np.where(mask == 127)] = 0
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel3)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel3)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel7)
+            #   grabMask, bgdModel, fgdModel = cv2.grabCut(img,mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
             cv2.imshow('Keypoints',mask)
-
             if paused:
                 delay = 0
             else:
@@ -117,8 +133,24 @@ for index in range(0, len(pointbuffer)):
     yline = axes[1].plot(t_c, y_c, 'o-', markersize=2)
 
 axes[1].invert_yaxis()
-plt.show()
+#plt.show()
 
+
+
+#img = cv2.imread('messi5.jpg')
+#mask = np.zeros(img.shape[:2],np.uint8)
+#bgdModel = np.zeros((1,65),np.float64)
+#fgdModel = np.zeros((1,65),np.float64)
+#rect = (50,50,450,290)
+#newmask = cv2.imread('newmask.png',0)
+# whereever it is marked white (sure foreground), change mask=1
+# whereever it is marked black (sure background), change mask=0
+#mask[newmask == 0] = 0
+#mask[newmask == 255] = 1
+#mask, bgdModel, fgdModel = cv2.grabCut(img,mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
+#mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+#img = img*mask[:,:,np.newaxis]
+#plt.imshow(img),plt.colorbar(),plt.show()
 
 print('You did the thing :)')
 
