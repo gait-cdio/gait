aacb7344b1823c522cae54307be722a348815492
Christoffer Malmgren <chrma018@student.liu.se>
2017-11-30 17:30:03 +0100

    Interpolation in position annotation

--- annotate_position.py
+++ annotate_position.py
@@ -2,6 +2,30 @@ import cv2
 import numpy as np
 import os
 
+
+def mouse_callback(event, x, y, flags, pos_anno):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if marked != 0:
+            pos_anno[frame, marked - 1] = (x, y)
+
+
+def interpolate_keypoints(pos_anno):
+    interpolated = np.copy(pos_anno)
+    nframes, nblobs, npos = pos_anno.shape
+    for blob in range(nblobs):
+        for pos in range(npos):
+            missing_frames = np.where(np.isnan(pos_anno[:, blob, pos]))[0]
+            existing_frames = np.where(~np.isnan(pos_anno[:, blob, pos]))[0]
+            if existing_frames.size:
+                interpolated[missing_frames, blob, pos] = np.interp(missing_frames,
+                                                                existing_frames, pos_anno[existing_frames, blob, pos],
+                                                                    left=np.nan, right=np.nan)
+    return interpolated
+
+
+if __name__ != "__main__":
+    exit(0)
+
 video_name = '4farger.mp4'
 cap = cv2.VideoCapture('input-videos/' + video_name)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -14,20 +38,13 @@ cv2.resizeWindow('Gait Annotator', width, height)
 
 frame = 0
 marked = 0
-pos_anno = np.zeros(shape=[4, nframes], dtype=(np.uint16, 2))
-
-
-def mouse_callback(event, x, y, flags, pos_anno):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if marked != 0:
-            pos_anno[marked - 1, frame] = (x, y)
-
+pos_anno = np.zeros(shape=[nframes, 4, 2], dtype=np.uint16) * np.nan
 
 w = cv2.namedWindow('Position Annotator', cv2.WINDOW_NORMAL)
 cv2.setMouseCallback('Position Annotator', mouse_callback, pos_anno)
 cv2.resizeWindow('Position Annotator', width, height)
 font = cv2.FONT_HERSHEY_TRIPLEX
-notations = ['emp', 'Left Toe', 'Left Heel', 'Right Toe', 'Right Heel']
+notations = ['empty', 'Left Toe', 'Left Heel', 'Right Toe', 'Right Heel']
 blob_color = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 0, 0)]
 
 loop = True
@@ -41,23 +58,25 @@ while loop:
     cv2.putText(im, 'Reset r', (0, 55), fontFace=font, fontScale=0.5, color=(0, 0, 255))
     cv2.putText(im, 'Save S (shift + s)', (0, 70), fontFace=font, fontScale=0.5, color=(0, 0, 255))
     cv2.putText(im, 'Mark: Mouseclick', (0, 85), fontFace=font, fontScale=0.5, color=(0, 0, 255))
-    cv2.putText(im, 'Change markers: i,k,o,l,p', (0, 100), fontFace=font, fontScale=0.5, color=(0, 0, 255))
-    cv2.putText(im, 'Frame:' + str(frame), (0, 115), fontFace=font, fontScale=0.5, color=(0, 0, 255))
+    cv2.putText(im, 'Interpolate: !', (0, 100), fontFace=font, fontScale=0.5, color=(0, 0, 255))
+    cv2.putText(im, 'Change markers: i,k,o,l,p', (0, 115), fontFace=font, fontScale=0.5, color=(0, 0, 255))
+    cv2.putText(im, 'Frame:' + str(frame), (0, 130), fontFace=font, fontScale=0.5, color=(0, 0, 255))
     if marked != 0:
         cv2.putText(im, notations[marked], (0, height - 40), fontFace=font, fontScale=3,
                     color=(0, 0, 255))
-        cv2.putText(im, "Last clicked position: {}, {}".format(*pos_anno[marked - 1, frame]), (0, height - 160),
+        cv2.putText(im, "Last clicked position: {}, {}".format(*pos_anno[frame, marked - 1]), (0, height - 160),
                     fontFace=font, fontScale=0.5, color=(0, 0, 255))
         for blob in range(4):
-            if not np.all(pos_anno[blob, frame] == 0):  # all zeros
-                cv2.circle(im, tuple(pos_anno[blob, frame]), 10, thickness=-1, color=blob_color[blob])
+            if not np.any(np.isnan(pos_anno[frame, blob])):  # Are you a nun?
+                cv2.drawMarker(im, tuple(pos_anno[frame, blob].astype(np.uint16)), thickness=2, markerSize=10,
+                               markerType=cv2.MARKER_TILTED_CROSS, color=blob_color[blob])
     cv2.imshow('Position Annotator', im)
     while True:
         pressed_key = cv2.waitKey(50) & 0xFF
         if pressed_key == 255:  # no key return
             break
         if pressed_key == ord('d'):
-            if frame < nframes:
+            if frame < nframes-1:
                 frame = frame + 1
                 ret, clean_im = cap.read()
         elif pressed_key == ord('a'):
@@ -72,7 +91,7 @@ while loop:
             cap.set(1, frame)
             ret, clean_im = cap.read()
         elif pressed_key == ord('r'):
-            pos_anno = np.zeros(shape=[4, nframes, 2], dtype=np.uint8)
+            pos_anno[:, :, :] = np.nan
         elif pressed_key == ord('i'):
             marked = 1
         elif pressed_key == ord('k'):
@@ -82,7 +101,9 @@ while loop:
         elif pressed_key == ord('l'):
             marked = 4
         elif pressed_key == ord('p'):
-            pos_anno[:, frame, :] = 0
+            pos_anno[frame, :, :] = 0
+        elif pressed_key == ord('!'):
+            pos_anno = interpolate_keypoints(pos_anno)
         elif pressed_key == ord('S'):
             np.save('annotations/' + os.path.splitext(video_name)[0] + '-positions', pos_anno)
         else:
@@ -107,13 +128,13 @@ while loop:
     cv2.putText(im, 'Frame:' + str(frame), (0, 55), fontFace=font, fontScale=0.5, color=(0, 0, 255))
 
     for blob in range(4):
-        if not np.all(p_anno[blob, frame] == 0):  # all zeros
-            cv2.circle(im, tuple(p_anno[blob, frame]), 10, thickness=-1, color=blob_color[blob])
+        if not np.any(np.isnan(p_anno[frame, blob])):
+            cv2.circle(im, tuple(p_anno[frame, blob]), 10, thickness=-1, color=blob_color[blob])
     cv2.imshow('Position Check', im)
     while True:
         pressed_key = cv2.waitKey(0) & 0xFF
         if pressed_key == ord('d'):
-            if frame < nframes:
+            if frame < nframes-1:
                 frame = frame + 1
                 ret, clean_im = cap.read()
         elif pressed_key == ord('a'):
