7f9773961eb5f0a7cedea9ec24dde00a287ba412
Kevin Kjellén <kevkj515@student.liu.se>
2017-12-01 13:25:37 +0100

    stride_parameters är mer rätt nu och kortare

--- stride_parameters.py
+++ stride_parameters.py
@@ -10,15 +10,16 @@ def write_gait_parameters_to_file(filename, updown_estimations, fps):
     feet_order=['left', 'right']
 
     for i, updown in enumerate(updown_estimations):
-        parameters['duty_cycle'][feet_order[i]] = duty_cycle(updown)
-        parameters['stance_duration'][feet_order[i]] = stance_duration(updown, fps)
-        parameters['swing_duration'][feet_order[i]] = swing_duration(updown, fps)
+        duty_c, stance_d, swing_d = duty_stance_swing(updown, fps)
+        parameters['duty_cycle'][feet_order[i]] = duty_c
+        parameters['stance_duration'][feet_order[i]] = stance_d
+        parameters['swing_duration'][feet_order[i]] = swing_d
 
     with open(filename, 'w') as f:
         yaml.dump(parameters, f, default_flow_style=False)
 
 
-def duty_cycle(updown_with_nans):
+def duty_stance_swing(updown_with_nans, fps):
     # Remove all values when foot is not in frame (i.e. nan values)
     updown = updown_with_nans[~np.isnan(updown_with_nans)]
 
@@ -28,9 +29,9 @@ def duty_cycle(updown_with_nans):
     last_transition = np.where(np.diff(updown) != 0)[0][-1] + 1
     clean_updown = updown[first_transition:last_transition]
 
-    # Calculate how many times the foot goes up respictevely down
-    n_up = sum(np.diff(clean_updown) == -1)
-    n_down = sum(np.diff(clean_updown) == 1)
+    # Calculate how many up cycles and down cycles there are
+    n_up = sum(np.diff(clean_updown) == -1) + (clean_updown[0] == 0)
+    n_down = sum(np.diff(clean_updown) == 1) + (clean_updown[0] == 1)
 
     # Count how many frames the foot is in the air/on the ground
     n_up_frames = sum(clean_updown == 0)
@@ -43,50 +44,10 @@ def duty_cycle(updown_with_nans):
     # Calculate the duty cycle = time_foot_is_down / total_time
     duty_cycle = mean_frames_per_down / (mean_frames_per_down + mean_frames_per_up)
 
-    return float(duty_cycle)
-
-def stance_duration(updown_with_nans, fps):
-    # Remove all values when foot is not in frame (i.e. nan values)
-    updown = updown_with_nans[~np.isnan(updown_with_nans)]
-
-    # Remove first incomplete steps, only consider values between first and last
-    # transition.
-    first_transition = np.where(np.diff(updown) != 0)[0][0] + 1
-    last_transition = np.where(np.diff(updown) != 0)[0][-1] + 1
-    clean_updown = updown[first_transition:last_transition]
-
-    # Calculate how many times the foot goes up respictevely down
-    n_up = sum(np.diff(clean_updown) == -1)
-    n_down = sum(np.diff(clean_updown) == 1)
-
-    # Count how many frames the foot is in the air/on the ground
-    n_up_frames = sum(clean_updown == 0)
-    n_down_frames = sum(clean_updown == 1) # will show one less than the amount of 'down' cycles, as the foot starts down in clean_updown
-
     # Calculate mean stance duration
-    stance_duration = float(n_down_frames) / float((n_down + 1) * fps)
-
-    return float(stance_duration)
-
-def swing_duration(updown_with_nans, fps):
-    # Remove all values when foot is not in frame (i.e. nan values)
-    updown = updown_with_nans[~np.isnan(updown_with_nans)]
-
-    # Remove first incomplete steps, only consider values between first and last
-    # transition.
-    first_transition = np.where(np.diff(updown) != 0)[0][0] + 1
-    last_transition = np.where(np.diff(updown) != 0)[0][-1] + 1
-    clean_updown = updown[first_transition:last_transition]
-
-    # Calculate how many times the foot goes up respictevely down
-    n_up = sum(np.diff(clean_updown) == -1)
-    n_down = sum(np.diff(clean_updown) == 1)
-
-    # Count how many frames the foot is in the air/on the ground
-    n_up_frames = sum(clean_updown == 0)
-    n_down_frames = sum(clean_updown == 1)
+    stance_duration = float(n_down_frames) / float((n_down) * fps)
 
     # Calculate mean swing duration
     swing_duration = float(n_up_frames) / float((n_up) * fps)
 
-    return float(swing_duration)
+    return float(duty_cycle), float(stance_duration), float(swing_duration)
