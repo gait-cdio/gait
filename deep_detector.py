9ea9c429204fd2653cf1ee4b9ef71e5cc796c44b
Rolf Lifvergren <rolli107@student.liu.se>
2017-12-06 10:49:22 +0100

    Refactor some stuff

--- deep_detector.py
+++ deep_detector.py
@@ -35,35 +35,7 @@ class ClickRecorder:
         plt.show()
 
 
-def selectHeel(image):
-    img = np.copy(image)
-    cv2.namedWindow("Bild")
-    roi = cv2.selectROI("Bild", img)
-    cv2.destroyAllWindows()
-    cv2.normalize(img, img, dtype=cv2.CV_32F)
-    x, y, w, h = roi
-    height, width, _ = img.shape
-    mask = np.zeros([height, width])
-    mask[y:y + h, x:x + w] = 1
-
-    heelArray = []
-    coordArray = []
-    for i in range(x - 15, x + w - 15):
-        for k in range(y - 15, y + h - 15):
-            heelArray.append(img[k:k + 32, i:i + 32])
-            coordArray.append((i + 15, k + 15))
-
-    falseArray = []
-    for i in range(x - 32, x + w):
-        falseArray.append(img[y - 32:y, i:i + 32])
-        falseArray.append(img[y + h:y + h + 32, i:i + 32])
-    for i in range(y - 32, y + h):
-        falseArray.append(img[i:i + 32, x - 32:x])
-        falseArray.append(img[i:i + 32, x + w:x + w + 32])
-    return np.array(heelArray), np.array(falseArray), coordArray
-
-
-def separateDatasets(inputs, outputs, ratio=0.6):
+def separate_dataset_into_train_val(inputs, outputs, ratio=0.6):
     num_inputs = inputs.shape[0]
     num_outputs = outputs.shape[0]
     assert num_inputs == num_outputs
@@ -91,60 +63,60 @@ def get_groundtruth_filename(video_filename):
     return groundtruth_filename
 
 
+def load_groundtruth(video_filename):
+    groundtruth_filename = get_groundtruth_filename(video_filename)
+
+    position_groundtruth = np.load(groundtruth_filename)
+
+    cap = cv2.VideoCapture(video_filename)
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    input_patches = np.zeros(shape=(num_frames * 4, 64, 64, 3))
+    output_patches = np.zeros(shape=(num_frames * 4, 64, 64))
+
+    sample_index = 0
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    for frame_index in range(num_frames):
+        if np.all(np.isnan(position_groundtruth[frame_index, :, 0])):
+            continue
+
+        # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index) # This one takes time and is not needed, I think
+        ret, image = cap.read()
+        assert ret
+
+        height, width, _ = image.shape
+        x, y = range(width), range(height)
+        xx, yy = np.meshgrid(x, y)
+        groundtruth_output = np.zeros(shape=(height, width))
+
+        sigma = 10.0
+        interesting_positions = position_groundtruth[frame_index][
+            np.where(~np.isnan(position_groundtruth[frame_index, :, 0]))]
+        interesting_positions = list(filter(lambda a: 40 < a[0] < width - 40, interesting_positions))
+        for pos in interesting_positions:
+            x, y = pos
+            groundtruth_output += np.exp(- ((xx - x) ** 2 + (yy - y) ** 2) / (2 * sigma ** 2))
+
+        for pos in interesting_positions:
+            x, y = int(pos[0]), int(pos[1])
+            input_patches[sample_index] = image[y - 32:y + 32, x - 32:x + 32]
+            output_patches[sample_index] = groundtruth_output[y - 32:y + 32, x - 32:x + 32]
+            sample_index += 1
+
+    return input_patches, output_patches
+
+
 video_filename = "input-images/rolf_markerless/rolf_markerless_%04d.jpg"
-groundtruth_filename = get_groundtruth_filename(video_filename)
-
-position_groundtruth = np.load(groundtruth_filename)
-
-cap = cv2.VideoCapture(video_filename)
-num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-positive_examples = np.zeros(shape=(num_frames * 4, 64, 64, 3))
-positive_example_outputs = np.zeros(shape=(num_frames * 4, 64, 64))
-
-sample_index = 0
-for frame_index in range(num_frames):
-    if np.all(np.isnan(position_groundtruth[frame_index, :, 0])):
-        continue
-
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-    ret, image = cap.read()
-    assert ret
-
-    height, width, _ = image.shape
-    x, y = range(width), range(height)
-    xx, yy = np.meshgrid(x, y)
-    groundtruth_output = np.zeros(shape=(height, width))
-
-    sigma = 10.0
-    interesting_positions = position_groundtruth[frame_index][np.where(~np.isnan(position_groundtruth[frame_index, :, 0]))]
-    interesting_positions = list(filter(lambda a: 40 < a[0] < width - 40, interesting_positions))
-    for pos in interesting_positions:
-        x, y = pos
-        groundtruth_output += np.exp(- ((xx - x) ** 2 + (yy - y) ** 2) / (2 * sigma ** 2))
-
-    for pos in interesting_positions:
-        x, y = int(pos[0]), int(pos[1])
-        positive_examples[sample_index] = image[y - 32:y + 32, x - 32:x + 32]
-        positive_example_outputs[sample_index] = groundtruth_output[y - 32:y + 32, x - 32:x + 32]
-        sample_index += 1
-
-positive_examples = positive_examples[:sample_index]
-positive_example_outputs = positive_example_outputs[:sample_index]
-
-cap.set(1, 30)
-ret, image = cap.read()
-
-assert ret
+positive_examples, positive_example_outputs = load_groundtruth(video_filename)
 
 np.random.shuffle(positive_examples)
 np.random.shuffle(positive_example_outputs)
 
-footDataset = separateDatasets(positive_examples, positive_example_outputs)
+foot_dataset = separate_dataset_into_train_val(positive_examples, positive_example_outputs)
 
-dataloaders = {x: torch.utils.data.DataLoader(footDataset[x], batch_size=50,
-                                              shuffle=True, num_workers=1)
-               for x in ['train', 'val']}
+data_loaders = {x: torch.utils.data.DataLoader(foot_dataset[x], batch_size=50,
+                                               shuffle=True, num_workers=1)
+                for x in ['train', 'val']}
 
 vgg = torchvision.models.vgg19(pretrained=True)
 vgg.features = torch.nn.Sequential(*[vgg.features[i] for i in range(4)])
@@ -158,27 +130,29 @@ if torch.cuda.is_available():
     vgg = vgg.cuda()
 
 criterion = torch.nn.MSELoss()
-optimizer_conv = optim.SGD(vgg.classifier.parameters(), lr=0.000001)
+optimizer_conv = optim.SGD(vgg.classifier.parameters(), lr=1e-6)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=1, gamma=0.1, last_epoch=-1)
 
-trained_model = test_dl_utils.train_model(vgg, criterion, optimizer_conv, exp_lr_scheduler, dataloaders,
+trained_model = test_dl_utils.train_model(vgg, criterion, optimizer_conv, exp_lr_scheduler, data_loaders,
                                           num_epochs=50)
 
+cap = cv2.VideoCapture(video_filename)
+
 for frame in [31, 61, 91]:
-    cap.set(1, frame)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
     ret, image = cap.read()
 
     assert ret
 
+    display_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     height, width = image.shape[0:2]
 
     fig, ax = plt.subplots()
-    ax.imshow(image)
+    ax.imshow(display_image)
     ax.set_title('Please select regions to evaluate')
     click_recorder = ClickRecorder(fig)
     plt.show()
-
-    display_image = np.copy(image)
 
     for index, coords in enumerate(click_recorder.clicks):
         x, y = int(coords[0]), int(coords[1])
