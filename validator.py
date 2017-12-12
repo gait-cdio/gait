6c94f3ae40cc11a5ee1757db8fcd43911046628e
Christoffer Malmgren <chrma018@student.liu.se>
2017-12-12 17:24:34 +0100

    Added false detections and missed detections as validation parameters

--- validator.py
+++ validator.py
@@ -6,7 +6,16 @@ def validate(ground_truth, estimation):
     diff = np.abs(ground_truth - estimation)
     diff_sum = np.nansum(diff)
     weight = diff.shape[1] - np.count_nonzero(np.isnan(diff))
-    return float(diff_sum/weight)
+    error_per_ok_frame=float(diff_sum/weight)
+
+    no_annotation = np.isnan(ground_truth)
+    no_estimation = np.isnan(estimation)
+    false_alarms = np.sum(np.logical_and(no_annotation, np.logical_not(no_estimation)))
+    missed_detections = np.sum(np.logical_and(np.logical_not(no_annotation), no_estimation))
+    false_alarm_per_frame = false_alarms/diff.shape[1]
+    missed_detection_per_frame = missed_detections/diff.shape[1]
+    return {'Error per correctly detected frame': error_per_ok_frame, 'False alarm per frame': false_alarm_per_frame,
+            'Missed detection per frame': missed_detection_per_frame}
 
 
 def error(ground_truth, estimation, nan_penalty):
