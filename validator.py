17158a203923efbbe6701f486277bc173010b631
Rolf Lifvergren <rolli107@student.liu.se>
2017-12-15 10:11:22 +0100

    Helper functions for calculating new error measurement (number of frames wrong)

--- validator.py
+++ validator.py
@@ -19,3 +19,47 @@ def validate(ground_truth, estimation):
         'False detections per frame': false_detection_per_frame,
         'Missed detections per frame': missed_detection_per_frame,
     }
+
+def find_ups(updown):
+    """ Take numpy array of up/down values, output indices where the feet leave the ground
+    """
+    ups = np.transpose(np.where(np.diff(updown) == -1))
+    return ups
+
+def find_downs(updown):
+    """ Take numpy array of up/down values, output indices where the feet start touching the ground
+    """
+    downs = np.transpose(np.where(np.diff(updown) == 1))
+    return downs
+
+def group_by_foot(indices):
+    result = [] 
+    for foot in np.unique(indices[:,0]):
+        indices_for_foot = indices[np.where(indices[:,0] == foot), 1]
+        result.append(indices_for_foot)
+    return result
+
+def get_frames(indices):
+    return indices[:, 1]
+
+def find_grouped_up_down_indices(updown):
+    return {
+        'ups': get_frames(find_ups(updown)),
+        'downs': get_frames(find_downs(updown)),
+    }
+
+def diff_closest(ind_a, ind_b):
+    mat1 = np.stack((ind_a,)*len(ind_b), axis=0)
+    mat2 = np.stack((ind_b.T,)*len(ind_a), axis=1)
+
+    longest_axis = np.argmax(mat1.shape)
+
+    absmat = np.abs(mat1 - mat2)
+    minimum_diffs = np.min(absmat, axis=longest_axis)
+
+    return minimum_diffs
+
+def load_est_and_gt(video_name):
+    est = np.load('output-data/{}_updown.npy'.format(video_name))
+    gt = np.load('annotations/{}-up_down.npy'.format(video_name))
+    return est, gt
