cd72973869008a21cfc09d02c6ce98e1bfbcf9d0
Rolf Lifvergren <rolli107@student.liu.se>
2017-12-15 10:57:27 +0100

    Calculate mean frame-error and include it in validation

--- validator.py
+++ validator.py
@@ -18,7 +18,9 @@ def validate(ground_truth, estimation):
         'Error per correctly detected frame': error_per_ok_frame,
         'False detections per frame': false_detection_per_frame,
         'Missed detections per frame': missed_detection_per_frame,
+        'Mean error per up/down transition': float(mean_frames_error(estimation=estimation, groundtruth=ground_truth)),
     }
+
 
 def find_ups(updown):
     """ Take numpy array of up/down values, output indices where the feet leave the ground
@@ -26,11 +28,13 @@ def find_ups(updown):
     ups = np.transpose(np.where(np.diff(updown) == -1))
     return ups
 
+
 def find_downs(updown):
     """ Take numpy array of up/down values, output indices where the feet start touching the ground
     """
     downs = np.transpose(np.where(np.diff(updown) == 1))
     return downs
+
 
 def group_by_foot(indices):
     result = [] 
@@ -39,27 +43,50 @@ def group_by_foot(indices):
         result.append(indices_for_foot)
     return result
 
+
 def get_frames(indices):
     return indices[:, 1]
 
-def find_grouped_up_down_indices(updown):
+
+def find_up_down_indices(updown):
     return {
         'ups': get_frames(find_ups(updown)),
         'downs': get_frames(find_downs(updown)),
     }
 
+
 def diff_closest(ind_a, ind_b):
+    """ Calculate the absolute difference of best fit between two 1d arrays
+
+    Smallest difference between each in one array to any in the other
+    """
     mat1 = np.stack((ind_a,)*len(ind_b), axis=0)
     mat2 = np.stack((ind_b.T,)*len(ind_a), axis=1)
 
-    longest_axis = np.argmax(mat1.shape)
+    shortest_axis = np.argmin(mat1.shape)
 
     absmat = np.abs(mat1 - mat2)
-    minimum_diffs = np.min(absmat, axis=longest_axis)
+    minimum_diffs = np.min(absmat, axis=shortest_axis)
 
     return minimum_diffs
+
 
 def load_est_and_gt(video_name):
     est = np.load('output-data/{}_updown.npy'.format(video_name))
     gt = np.load('annotations/{}-up_down.npy'.format(video_name))
     return est, gt
+
+
+def mean_frames_error(estimation, groundtruth):
+    indices_est = find_up_down_indices(estimation)
+    indices_gt = find_up_down_indices(groundtruth)
+
+    up_diffs = diff_closest(indices_est['ups'], indices_gt['ups'])
+    down_diffs = diff_closest(indices_est['downs'], indices_gt['downs'])
+
+    mean_error = (sum(up_diffs) + sum(down_diffs)) / (len(up_diffs) + len(down_diffs))
+    return mean_error
+
+
+if __name__ == '__main__':
+    mean_frames_error(*load_est_and_gt('4markersjohn1'))
